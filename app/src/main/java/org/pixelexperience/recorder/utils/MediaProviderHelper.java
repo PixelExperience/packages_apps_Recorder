b599cde7c87110fbe03c7697ec9533a527797d01
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-06-02 15:16:50 -0300

    Improve video writting process
    Prevents OOM
    
    Change-Id: I328040487092b7bcac5d6ece8e925996faa52350

--- app/src/main/java/org/pixelexperience/recorder/utils/MediaProviderHelper.java
+++ app/src/main/java/org/pixelexperience/recorder/utils/MediaProviderHelper.java
@@ -3,18 +3,14 @@ package org.pixelexperience.recorder.utils;
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.net.Uri;
-import android.os.AsyncTask;
-import android.os.ParcelFileDescriptor;
 import android.provider.MediaStore;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.OutputStream;
 import java.nio.file.Files;
 
 public final class MediaProviderHelper {
@@ -23,15 +19,15 @@ public final class MediaProviderHelper {
     private MediaProviderHelper() {
     }
 
-    public static void addVideoToContentProvider(
+    public static String addVideoToContentProvider(
             @Nullable ContentResolver cr,
-            @Nullable File file,
-            @NonNull OnContentWritten listener) {
+            @Nullable File tempFile,
+            @Nullable File file) {
         if (cr == null || file == null) {
-            return;
+            return null;
         }
 
-        final ContentValues values = new ContentValues();
+        ContentValues values = new ContentValues();
         values.put(MediaStore.Video.Media.DISPLAY_NAME, file.getName());
         values.put(MediaStore.Video.Media.TITLE, file.getName());
         values.put(MediaStore.Video.Media.MIME_TYPE, "video/mp4");
@@ -41,12 +37,27 @@ public final class MediaProviderHelper {
 
         final Uri uri = cr.insert(MediaStore.Video.Media.getContentUri(
                 MediaStore.VOLUME_EXTERNAL_PRIMARY), values);
+
         if (uri == null) {
             Log.e(TAG, "Failed to insert " + file.getAbsolutePath());
-            return;
+            return null;
         }
 
-        new WriterTask(file, uri, cr, listener).execute();
+        try (OutputStream os = cr.openOutputStream(uri, "w")) {
+            assert os != null;
+            Files.copy(tempFile.toPath(), os);
+            tempFile.delete();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.e(TAG, "Failed to insert " + file.getAbsolutePath());
+            return null;
+        }
+
+        values = new ContentValues();
+        values.put(MediaStore.MediaColumns.IS_PENDING, 0);
+        cr.update(uri, values, null, null);
+
+        return uri.toString();
     }
 
     public static void remove(@NonNull ContentResolver cr, @NonNull Uri uri) {
@@ -54,65 +65,6 @@ public final class MediaProviderHelper {
             cr.delete(uri, null, null);
         } catch (Exception ignored) {
 
-        }
-    }
-
-    public interface OnContentWritten {
-        void onContentWritten(@Nullable String uri);
-    }
-
-    @RequiresApi(29)
-    static class WriterTask extends AsyncTask<Void, Void, String> {
-        @NonNull
-        private final File file;
-        @NonNull
-        private final Uri uri;
-        @NonNull
-        private final ContentResolver cr;
-        @NonNull
-        private final OnContentWritten listener;
-
-        /* synthetic */ WriterTask(@NonNull File file,
-                                   @NonNull Uri uri,
-                                   @NonNull ContentResolver cr,
-                                   @NonNull OnContentWritten listener) {
-            this.file = file;
-            this.uri = uri;
-            this.cr = cr;
-            this.listener = listener;
-        }
-
-        @Override
-        protected String doInBackground(Void... voids) {
-            try {
-                final ParcelFileDescriptor pfd = cr.openFileDescriptor(uri, "w", null);
-                if (pfd == null) {
-                    return null;
-                }
-                final FileOutputStream oStream = new FileOutputStream(pfd.getFileDescriptor());
-                oStream.write(Files.readAllBytes(file.toPath()));
-                oStream.close();
-                pfd.close();
-
-
-                final ContentValues values = new ContentValues();
-                values.put(MediaStore.MediaColumns.IS_PENDING, 0);
-                cr.update(uri, values, null, null);
-
-                if (!file.delete()) {
-                    Log.w(TAG, "Failed to delete tmp file");
-                }
-
-                return uri.toString();
-            } catch (IOException e) {
-                Log.e(TAG, "Failed to write into MediaStore", e);
-                return null;
-            }
-        }
-
-        @Override
-        protected void onPostExecute(String s) {
-            listener.onContentWritten(s);
         }
     }
 }
